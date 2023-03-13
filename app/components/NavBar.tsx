ca78bb59b1c690a8225d89536555516edaa63936
ibrhmvk <ibrahimvk96@gmail.com>
2023-03-13 17:38:42 +0530

    refactoring

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -7,23 +7,21 @@ import { AuthenticationContext } from "../context/AuthContext";
 import useAuth from "../../hooks/useAuth";
 
 export default function NavBvar() {
-  const { data, setAuthState, loading } = useContext(AuthenticationContext)
-  const {logOut} = useAuth()
-  const handleLogOut = () => {
-    logOut()
-  }
+  const { data, loading } = useContext(AuthenticationContext)
+  const { logOut } = useAuth()
+
   return (
     <nav className="bg-white p-2 flex justify-between">
       <Link href="" className="font-bold text-gray-700 text-2xl"> OpenTable </Link>
       <div>
-        {loading ? null : (<div className="flex">
-          {data ? <button onClick={handleLogOut} className="p-1 px-4 rounded border">Log Out</button> :
+        <div className="flex">
+          {data ? <button onClick={logOut} className="p-1 px-4 rounded border">Log Out</button> :
             <>
               <AuthModal />
               <AuthModal isSignUp />
             </>
           }
-        </div>)}
+        </div>
       </div>
     </nav>
   )
